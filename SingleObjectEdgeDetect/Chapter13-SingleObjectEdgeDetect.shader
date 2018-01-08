1d25810c1f4407f871152d60bc10d87468f8945f
DESKTOP-0D8KPI9\14005 <1400539511@qq.com>
2018-01-08 21:49:50 +0800

    功能：单个物体边缘检测进行描边 功能组成：脚本加SHADER单个物体边缘检测

---
+++ SingleObjectEdgeDetect/Chapter13-SingleObjectEdgeDetect.shader
@@ -0,0 +1,67 @@
+﻿//简介：在阈值范围内检测边缘，只用单一颜色渲染边缘，不是边缘的剔除不予渲染
+//特点：以像素为单位渲染
+//用途：物体被选中的边缘高亮效果
+//使用原理：在高亮物体的位置，再次渲染它的模型，用此shader，渲染边缘，中间不渲染露出原来的模型
+Shader "Unity Shaders Book/Chapter 13/SingleObjectEdgeDetect" {
+	Properties
+	{
+
+		_EdgeColor("Edge Color", Color) = (0, 0, 0, 1)
+		_Threshold("Threshold",Range(0,1)) = 0.5
+	}
+
+	SubShader
+	{
+		CGINCLUDE
+		#include "UnityCG.cginc"
+		#include "Lighting.cginc"
+
+		fixed4 _EdgeColor;
+		fixed _Threshold;
+
+		struct a2v
+		{
+			float4 vertex : POSITION;
+			float3 normal : NORMAL;
+		};
+
+		struct v2f {
+			float4 pos : SV_POSITION;
+			float3 worldNormal : TEXCOORD0;
+			float3 worldViewDir : TEXCOORD1;
+		};
+
+		v2f vert(a2v v) {
+			v2f o;
+			o.pos = UnityObjectToClipPos(v.vertex);
+
+			o.worldNormal = mul(v.normal, (float3x3)unity_WorldToObject);
+			o.worldViewDir = UnityWorldSpaceViewDir(mul(unity_ObjectToWorld, v.vertex).xyz);
+			return o;
+		}
+
+		fixed4 fragRobertsCrossDepthAndNormal(v2f i) : SV_Target
+		{
+			fixed3 worldNormal = normalize(i.worldNormal);
+			fixed3 worldViewDir = normalize(i.worldViewDir);
+			clip(_Threshold -abs(dot(worldNormal,worldViewDir)));
+
+			return _EdgeColor;
+		}
+
+
+		ENDCG
+
+		Pass
+		{
+			//ZTest Always Cull Off ZWrite Off
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment fragRobertsCrossDepthAndNormal
+
+			ENDCG
+		}
+	}
+	Fallback Off
+}
---
+++ SingleObjectEdgeDetect/SingleEdgeDetect.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SingleEdgeDetect : MonoBehaviour {
+
+    public Shader edgeDetectShader;
+    private Material edgeDetectMaterial = null;
+    public Material material
+    {
+        get
+        {
+            edgeDetectMaterial = CheckShaderAndCreateMaterial(edgeDetectShader, edgeDetectMaterial);
+            return edgeDetectMaterial;
+        }
+    }
+
+
+    public Color edgeColor = Color.black;
+    [Range(0f, 1f)]
+    public float threshold = 0.5f;
+
+    private Mesh mesh;
+    private void Awake()
+    {
+        if (GetComponent<MeshFilter>())
+            mesh = GetComponent<MeshFilter>().sharedMesh;
+        else
+        {
+            Debug.LogError("GameObject'" + gameObject.name + "'没有MESH,此脚本无效");
+            this.enabled = false;
+        }
+    }
+
+    //[ImageEffectOpaque]
+    public void Update()
+    {
+        if (material != null)
+        {
+            material.SetColor("_EdgeColor", edgeColor);
+            material.SetFloat("_Threshold", threshold);
+
+            Graphics.DrawMesh(mesh, Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale), material,1);
+        }
+    }
+
+    protected Material CheckShaderAndCreateMaterial(Shader shader, Material material)
+    {
+        if (shader == null)
+        {
+            return null;
+        }
+
+        if (shader.isSupported && material && material.shader == shader)
+            return material;
+
+        if (!shader.isSupported)
+        {
+            return null;
+        }
+        else
+        {
+            material = new Material(shader);
+            material.hideFlags = HideFlags.DontSave;
+            if (material)
+                return material;
+            else
+                return null;
+        }
+    }
+}
